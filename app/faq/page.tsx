89b7fc850ce9d42b3087002d27bea44334ca196e
kirannandedkar <kirannandedkar@yahoo.com>
2024-07-01 11:16:05 +0200

    added faq

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -5,26 +5,30 @@ import FaqPane from "@/app/faq/FaqPane";
 
 const faqs = [
     {
-        question: "What do you mean by \"Figma assets\"?",
-        answer: "You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.",
+        question: "Will the non members do not get an invitation for upcoming events",
+        answer: "Everyone will get invitation to upcoming events if you are registered in our google group. You can do so by sending email to our committe at mmn@marathimandalnorway.no . You can also be part of whatsapp group by sending your no to said email",
     },
     {
-        question: "What do you mean by \"Figma assets\"?",
-        answer: "You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.",
+        question: "When are AGM (Annual general meetings) held?",
+        answer: "AGM are held every 2 years.",
     },
     {
-        question: "What do you mean by \"Figma assets\"?",
-        answer: "You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.",
+        question: "Is AGM online or physically held?",
+        answer: "If we get place then we will held it physically otherwise will be online.",
     },
     {
-        question: "What do you mean by \"Figma assets\"?",
-        answer: "You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.",
+        question: "Where can i find the constitution?",
+        answer: "You can find constitution under the menu AboutUs> MMN Constitution or by just clicking here https://maharashtramandalnorway.no/constitution.pdf",
     }, {
-        question: "What do you mean by \"Figma assets\"?",
-        answer: "You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.",
+        question: "Where can i ask if we have any general questions. Do you have any open forum?",
+        answer: "You can ask questions during Annual general meeting or you can send email to mmn@marathimandalnorway.no . Apart from it there is no open forum. This is because we are frivillig organisation. Every Organisation always ask their paid members to contact on organisation mail Id for their open question doubts suggestion feedbacks.For eg. Turn forening, swimming, any Gym members has to ask questions by sending email to support. "
     }, {
-        question: "What do you mean by \"Figma assets\"?",
-        answer: "You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.",
+        question: "What is yearly membership cost?",
+        answer: "yearly membership is 160 NOK per person for all above 16 years."
+    }    
+    , {
+        question: "Do you have half yearly or quarterly membership?",
+        answer: "No as per now we have only yearly membership and we might think of having half yearly or quarterly membership in upcoming years"
     }
 ]
 
