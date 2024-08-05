3cdecd671f22b030297532c5fc4b98a867c165d9
kirannandedkar <kirannandedkar@yahoo.com>
2024-08-05 11:09:04 +0200

    Update page.tsx
    minor text change

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -13,7 +13,7 @@ const DescriptionItems = [
     { title: `Payment`, description: `At the checkout, you can pay with VISA/MASTERCARD and VIPPS. When you shop with a payment card at MaharashtraMandalNorway.no, the payment is processed by NETS/Netaxept, a secure electronic payment solution for Visa and MasterCard. All card information is stored in accordance with the card companies' regulations. All information you provide will be encrypted directly between you as a customer and our payment partner NETS/Netaxept. We use NETS' own checkout solution "Nets Easy".` },
     { title: `Prices`, description: `All prices are in Norwegian kroner and are shown including VAT.` },
     { title: `Personal information`, description: `MaharashtraMandalNorway.no processes personal data in accordance with the Personal Data Act. Information that can be linked to you as a person will never be made available to other businesses or linked with other external registers.` },
-    { title: `Membership`, description: `MaharashtraMandalNorway is frivillig organisation registered in brønnøysundregisteret with org no 986 130 608. We dont sell any products. It is only possible to be members and membership per person is ${DefaultMemberFee} NOK per month.` },
+    { title: `Membership`, description: `MaharashtraMandalNorway is frivillig organisation registered in brønnøysundregisteret with org no 986 130 608. We dont sell any products. It is only possible to be members and membership per person is ${DefaultMemberFee} NOK per year.` },
     { title: `Contact information`, description: `For any question please email us at mmn@maharashtramandalnorway.no.` },
 ];
 
