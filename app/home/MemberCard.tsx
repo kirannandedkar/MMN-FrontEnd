d32a02ff548ca275bc73c8d8b6f782967c0c4b5c
kirannandedkar <kirannandedkar@yahoo.com>
2024-08-11 18:33:15 +0200

    Update MemberCard.tsx
    updated text

--- app/home/MemberCard.tsx
+++ app/home/MemberCard.tsx
@@ -35,7 +35,33 @@ const MemberCard = () => {
             </div>
 
             <div className="line-height-mmn-large text-white">
-                MMN Membership goes from January to December of each year. Do check our membership benefits and join us.
+                "use client"
+
+import MMNTitle from "@/components/MMNTItle";
+import MMNPanel from "@/components/MMNPanel";
+import MMNButton from "@/components/MMNButton";
+
+export default function EventPane() {
+    return (
+        <MMNPanel className="bg-mmn-red">
+            <MMNTitle title="Become a member" color="purple" />
+            <div className="line-height-mmn-normal text-white">
+                Price:
+                <div className="inline text-red-500 bg-white mx-[8px] px-[8px] py-[4px] rounded-[3px]">kr 150</div>
+                per member
+            </div>
+
+            <div className="line-height-mmn-large text-white">
+                MMN Membership is valid for the calendar year, starting from January 1st and ending on December 31st. Regardless of when you join during the year, your membership will be active only until the end of December. 
+                To continue enjoying the benefits of MMN Membership, renewal is required at the beginning of each new year.
+            </div>
+
+            <div className="flex justify-end">
+                <MMNButton title="Become a member" color="white" />
+            </div>
+        </MMNPanel>
+    )
+}
             </div>
 
             <div className="flex justify-end" onClick={() => router.push("/membership")}>
