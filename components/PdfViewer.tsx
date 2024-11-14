dbc82ece2ef3cc19e50259fe6eeced37140e1eb9
Md. Alam Hossain <alam.rpi.bd@gmail.com>
2024-11-14 21:39:08 +0600

    magazine page changed

--- components/PdfViewer.tsx
+++ components/PdfViewer.tsx
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { pdfToPages } from '@/utils/funcs'; // Assuming this is your helper file
-import HTMLFlipBook from 'react-pageflip'; // Ensure you are using the correct component import
-import Loader from './Loader';
+// import React, { useEffect, useState } from 'react';
+// import { pdfToPages } from '@/utils/funcs'; // Assuming this is your helper file
+// import HTMLFlipBook from 'react-pageflip'; // Ensure you are using the correct component import
+// import Loader from './Loader';
+// import { set } from 'lodash';
 
-interface FlipbookProps {
-  pdfFile: string;
-}
+// interface FlipbookProps {
+//   pdfFile: string;
+// }
 
-const Flipbook: React.FC<FlipbookProps> = ({ pdfFile }) => {
-  const [pages, setPages] = useState<string[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+// const Flipbook: React.FC<FlipbookProps> = ({ pdfFile }) => {
+//   const [pages, setPages] = useState<string[]>([]);
+//   const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const loadPdf = async () => {
-      try {
-        const pageImages = await pdfToPages(pdfFile);
-        const imageUrls = pageImages.map(page => page.url);
-        setPages(imageUrls);
-      } catch (error) {
-        console.error('Error loading PDF:', error);
-      }
-    };
+//   useEffect(() => {
+//     const loadPdf = async () => {
+//       try {
+//         const pageImages = await pdfToPages(pdfFile);
+//         const imageUrls = pageImages.map(page => page.url);
+//         setPages(imageUrls);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Error loading PDF:', error);
+//       }
+//     };
 
-    loadPdf();
-  }, [pdfFile]);
+//     loadPdf();
+//   }, [pdfFile]);
 
-  if(loading)
-    return <Loader/>
+//   if(loading)
+//     return <Loader/>
     
-  return (
-    <HTMLFlipBook
-      width={600}
-      height={800}>
-      {pages.map((url, index) => (
-        <div key={index} className="page">
-          <img src={url} alt={`Page ${index + 1}`} style={{ width: '100%' }} />
-        </div>
-      ))}
-    </HTMLFlipBook>
-  );
-};
+//   return (
+//     <HTMLFlipBook
+//     showCover={true}
+//      size={'stretch'}
+//       width={600}
+//       height={800}>
+//       {pages.map((url, index) => (
+//         <div key={index} className="page">
+//           <img src={url} alt={`Page ${index + 1}`} style={{ width: '100%' }} />
+//         </div>
+//       ))}
+//     </HTMLFlipBook>
+//   );
+// };
 
-export default Flipbook;
+// export default Flipbook;
