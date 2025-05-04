4ba877c9705d4025251a43b93915ae15b720d9e6
kirannandedkar <kirannandedkar@yahoo.com>
2025-05-04 09:20:18 +0200

    Update page.tsx
    Changed description

--- app/initiative/marathiboli-shaala/page.tsx
+++ app/initiative/marathiboli-shaala/page.tsx
@@ -6,7 +6,7 @@ import MMNTitle from "@/components/MMNTItle";
 const NavData = [
   { title: "Home", link: "/home" },
   { title: "MMN Initiatives", link: "#" },
-  { title: "MarathiBoli Shaala", link: "#" },
+  { title: "Norway Marathi Shaala", link: "#" },
 ];
 
 const MarathiBoliShaalaPage = () => {
@@ -15,13 +15,14 @@ const MarathiBoliShaalaPage = () => {
       <TopNav itemList={NavData} />
       <MMNContainer className="gap-[40px] pb-[40px] lg:flex-row flex-col">
         <div className="min-h-screen flex flex-col bg-white">
-          <video style={{width: '100%', height: '50%'}} controls>
+            {/* <video style={{width: '100%', height: '50%'}} controls>
             <source src="/image/marathi-boli/intro.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+           */}
           
           <MMNTitle
-            title="MarathiBoli Marathi Shaala"
+            title="Norway Marathi Shaala"
             className="pb-2 mb-2"
             color="purple"
           />
@@ -30,7 +31,7 @@ const MarathiBoliShaalaPage = () => {
             Purposes and goals of the program:
           </p>
           <p className="text-gray-700 mb-4">
-            The purpose of the MarathiBoli Shala is to encourage
+            The purpose of the Norway Marathi Shala is to encourage
             children to speak in Marathi. Children should have the ability to
             conduct meaningful conversations in Marathi due to exposure to the
             language in the classroom and at home. In addition to speaking
@@ -39,7 +40,7 @@ const MarathiBoliShaalaPage = () => {
           </p>
           <br />
           <p className="text-gray-700 mb-4">
-            The MarathiBoli Marathi Shala is operated by Maharashtra Mandal
+            The Norway Marathi Shala is operated by Maharashtra Mandal
             Norway, a Oslo-based non-profit organization that encourages Marathi
             cultural and language education among Norway-born Indian children.
           </p>
